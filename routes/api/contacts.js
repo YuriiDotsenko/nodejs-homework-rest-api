1b8ca2a4481244287b4bf57edfafb2966803899c
YuriiDotsenko <yuriidotsenko59@gmail.com>
2021-10-21 14:59:01 +0200

    дз

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,22 +1,24 @@
 const express = require("express");
 
 const { controllerWrapper, validation } = require("../../middlewares");
-const { contactSchema } = require("../../schema");
+const { joiSchema } = require("../../models/contact");
 const { contacts: ctrl } = require("../../controllers");
 
 const router = express.Router();
 
-router.get("/", ctrl.getAll);
+router.get("/", controllerWrapper(ctrl.getAll));
 
 router.get("/:contactId", controllerWrapper(ctrl.getById));
 
-router.post("/", validation(contactSchema), controllerWrapper(ctrl.add));
+router.post("/", validation(joiSchema), controllerWrapper(ctrl.add));
 
 router.put(
   "/:contactId",
-  validation(contactSchema),
+  validation(joiSchema),
   controllerWrapper(ctrl.updateById)
 );
+
+router.patch("/:contactId", controllerWrapper(ctrl.updateStatusContact));
 
 router.delete("/:contactId", controllerWrapper(ctrl.removeById));
 
