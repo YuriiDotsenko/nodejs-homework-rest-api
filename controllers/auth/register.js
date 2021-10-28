6ae6269fd6b1c261c04abf81fb0a8ea923ecea7a
YuriiDotsenko <yuriidotsenko59@gmail.com>
2021-10-28 13:40:49 +0200

    зробив дз

--- controllers/auth/register.js
+++ controllers/auth/register.js
@@ -2,8 +2,10 @@ const gravatar = require("gravatar");
 const { BadRequest, Conflict } = require("http-errors");
 const { joiSchema } = require("../../models/user");
 const bcrypt = require("bcryptjs");
+const { nanoid } = require("nanoid");
 
 const { User } = require("../../models");
+const { sendEmail } = require("../../helpers");
 
 const register = async (req, res, next) => {
   try {
@@ -20,20 +22,37 @@ const register = async (req, res, next) => {
 
     const avatarURL = gravatar.url(email);
 
+    const verificationToken = nanoid();
+
     const hashPassword = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
     const result = await User.create({
       email,
       password: hashPassword,
       avatarURL,
+      verificationToken,
       subscription,
     });
     const { subscription: subscr, _id } = result;
+
+    const mail = {
+      to: email,
+      subject: "Підтвердження реєстрації на сайті",
+      html: `<a target="_blank" href="http://localhost:3000/api/users/verify/${verificationToken}">Натисніть для підтвердження email</a>`,
+    };
+
+    sendEmail(mail);
 
     res.status(201).json({
       status: "success",
       code: 201,
       message: "Register success",
-      user: { email, avatarURL, subscription: subscr, _id },
+      user: {
+        email,
+        avatarURL,
+        verificationToken,
+        subscription: subscr,
+        _id,
+      },
     });
   } catch (error) {
     next(error);
